410d876d71a48fd4d8c6c54c4b9858a705ee8ac4
Jordan Harris <jharris@wework.com>
2018-06-18 21:26:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,3 +14,13 @@ def position_taken?(board, index)
     else true
   end
 end
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+    output = " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+    puts output
+end
+
+display_board(board)
+
